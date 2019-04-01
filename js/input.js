6f0c52e2b54a34f68a5361f1dd949c42f31eca3c
fangjianhui <fangjianhui77@163.com>
2019-04-01 18:06:18 +0800

    规范代码

--- js/input.js
+++ js/input.js
@@ -1,85 +1,84 @@
 $(function(){
+  //input只是一个函数，想让它代表页面中某一个input，所以用选择器告诉他
+  window.Input = function (selector) {
+    var $ele,
+        $error_ele,
+        me = this,
+        rule = {
+          nullable: true
+        };
 
-    //input只是一个函数，想让它代表页面中某一个input，所以用选择器告诉他
-    window.Input = function (selector) {
-        var $ele,
-            $error_ele,
-            me = this,
-            rule = {
-                nullable: true
-            };
-
-        //规则解析完后，为了检测是否合法，把它传到validator
-        this.load_validator = function() {
-            var val = this.get_val();
-            this.validator = new Validator(val,rule);
-        }
-
-        //rule和val 都要获取
-        this.get_val = function() {
-            return $ele.val();
-        }
-
-        function init() {
-           find_ele();
-           get_error_ele();
-           parse_rule();
-           me.load_validator();
-           listen();
-        }
-
-        function listen() {
-            $ele.on('blur',function(){
-                var valid = me.validator.is_valid(me.get_val());
-                //console.log('valid:',r);
-                if(valid){
-                     $error_ele.hide();
-                 }else{
-                     $error_ele.show();
-                 }
-            })
-        }
-
-        //用于显示错误
-        function get_error_ele() {
-            $error_ele = $(get_error_selector());
-        }
-
-        function get_error_selector() {
-            return '#' + $ele.attr('name') + '-input-error';
-        }
-
-        function find_ele() {
-            if (selector instanceof jQuery){
-                $ele = selector;
-            }else{
-                $ele = $(selector);
-            }  
-        }
-
-        //解析input规则
-        function parse_rule() {
-            var i;
-            var rule_str = $ele.data('rule');
-            if (!rule_str) return;
-
-            var rule_arr = rule_str.split('|');
-            /*转换为：
-            [
-                'pattern:^[.a-z0-9]$',
-                'maxlength:10',
-                'minlength:2',
-                'nullable:false',
-                'numeric:false'
-            ]
-            */
-           for(i=0;i<rule_arr.length;i++){
-               var item_str = rule_arr[i];
-               var item_arr = item_str.split(':');
-               rule[item_arr[0]] = JSON.parse(item_arr[1]);//用JSON.parse更严谨
-           }
-        }
-
-        init();
+    //规则解析完后，为了检测是否合法，把它传到validator
+    this.load_validator = function() {
+      var val = this.get_val();
+      this.validator = new Validator(val, rule);
     }
+
+    //rule和val 都要获取
+    this.get_val = function() {
+      return $ele.val();
+    }
+
+    function init() {
+      find_ele();
+      get_error_ele();
+      parse_rule();
+      me.load_validator();
+      listen();
+    }
+
+    function listen() {
+      $ele.on('blur', function() {
+        var valid = me.validator.is_valid(me.get_val());
+        //console.log('valid:', r);
+        if (valid) {
+          $error_ele.hide();
+        } else {
+          $error_ele.show();
+        }
+      })
+    }
+
+    //用于显示错误
+    function get_error_ele() {
+      $error_ele = $(get_error_selector());
+    }
+
+    function get_error_selector() {
+      return '#' + $ele.attr('name') + '-input-error';
+    }
+
+    function find_ele() {
+      if (selector instanceof jQuery) {
+        $ele = selector;
+      } else {
+        $ele = $(selector);
+      }  
+    }
+
+    //解析input规则
+    function parse_rule() {
+      var i;
+      var rule_str = $ele.data('rule');
+      if (!rule_str) return;
+      var rule_arr = rule_str.split('|');
+      /*转换为：
+      [
+        'pattern:^[.a-z0-9]$',
+        'maxlength:10',
+        'minlength:2',
+        'nullable:false',
+        'numeric:false'
+      ]
+      */
+      for (i = 0; i < rule_arr.length; i++) {
+        var item_str = rule_arr[i];
+        var item_arr = item_str.split(':');
+        rule[item_arr[0]] = JSON.parse(item_arr[1]);
+        //用JSON.parse更严谨
+      }
+    }
+
+    init();
+  }
 })
--- js/main.js
+++ js/main.js
@@ -1,115 +1,118 @@
 $(function(){
-    'use strict';
+  'use strict';
 
-// 选中所有input[data-rule]
-    var $inputs = $('[data-rule]');//选中要验证的input
-    var $form = $('#signup');
-    var inputs = [];
-    
-    //jquery的each方法
-    $inputs.each(function(index,node){
-        // 解析每一个input验证规则
-        var tmp = new Input(node);
-        inputs.push(tmp);
-    })
+  // 选中所有要验证input[data-rule]
+  var $inputs = $('[data-rule]');
+  var $form = $('#signup');
+  var inputs = [];
+  
+  // jquery的each方法
+  $inputs.each(function(index,node) {
+    // 解析每一个input验证规则
+    var tmp = new Input(node);
+    inputs.push(tmp);
+  })
 
-    //console.log('inputs:',inputs);
-    $form.on('submit',function(e){
-        e.preventDefault();
-        $inputs.trigger('blur');
-        for(var i=0;i<inputs.length;i++){
-            var item = inputs[i];
-            var r = item.validator.is_valid();
-            if(!r){
-                alert('invalid');
-                return;
-            }
-        }
-
-        //signup();
-        alert('valid');
-    })
-
-    function signup() {
-        $.post(url,{
-            name:'',
-
-        })
+  // console.log('inputs:',inputs);
+  $form.on('submit', function(e) {
+    e.preventDefault();
+    $inputs.trigger('blur');
+    for (var i = 0; i < inputs.length; i++) {
+      var item = inputs[i];
+      var r = item.validator.is_valid();
+      if(!r){
+        alert('invalid');
+        return;
+      }
     }
+
+    // signup();
+    alert('valid');
+  })
+
+  function signup() {
+    $.post(url,{
+      name:''
+    })
+  }
 
 // 验证
 /* var validator = new Validator('asdasd,asdasd.',{
-        maxlength: 9,
-        minlength: 2,
-        pattern:'^[.,a-zA-Z0-9]*$',//*表示可以为空字符串，页面有判断不能为空，所以这里留*；+表示不可为空
-    });
+      maxlength: 9,
+      minlength: 2,
+      pattern:'^[.,a-zA-Z0-9]*$',//*表示可以为空字符串，页面有判断不能为空，所以这里留*；+表示不可为空
+  });
 */
-    // var result = validator.validate_maxlength();
-    // var result = validator.validate_minlength();
-    // var result = validator.validate_numeric();
-    // var result = validator.validate_nullable();
-    // var result = validator.validate_pattern();
-    // console.log('result',result);
+  // var result = validator.validate_maxlength();
+  // var result = validator.validate_minlength();
+  // var result = validator.validate_numeric();
+  // var result = validator.validate_nullable();
+  // var result = validator.validate_pattern();
+  // console.log('result',result);
 
-    // var test = new Input('#test');
-    // var valid = test.validator.is_valid();
-    // console.log('valid:',valid);
+  // var test = new Input('#test');
+  // var valid = test.validator.is_valid();
+  // console.log('valid:',valid);
 
+  // 创建一个连接，这里的参数是服务端的链接
+  var socket = new WebSocket(url);
 
-    //创建一个连接，这里的参数是服务端的链接
-    var socket = new WebSocket(url);
-    
-    function emit() {
-        
-        var text = $("#msg").val();
-        var msg = {
-            "message" : text,
-            "color" : "#CECECE",
-            "bubbleColor" : "#2E2E2E",
-            "fontSize" : "14",
-            "fontType" : "微软雅黑"
-        };
-        msg = JSON.stringify(msg);
-        //向服务端发送消息
-        socket.send(msg);
-    
-        //将自己发送的消息内容静态加载到html上，服务端实现自己发送的消息不会推送给自己
-        $("#content").append("<kbd style='color: #" + "CECECE" + ";float: right;clear: right; font-size: " + 12 + ";'>" + text +  "</kbd><br/>");
-        //将消息文本框清空
-        $("#msg").val("");
-    } 
-
-
-    $(function() {
-        //初始化加载listen方法
-        listen();
-    })
-    
-    function listen() {
-        //打开连接时触发
-        socket.onopen = function() {
-            $("#content").append("<kbd>Welcome!</kbd></br>");
-        };
-        //收到消息时触发
-        socket.onmessage = function(evt) {
-            var data = JSON.parse(evt.data);
-            $("#content").append("<kbd style='color: #" + data.color + ";font-size: " + data.fontSize + ";margin-top: 10px;'>" + data.userName + " Say: " + data.message + "</kbd></br>");
-        };
-        //关闭连接时触发
-        socket.onclose = function(evt) {
-            $("#content").append("<kbd>" + "Close!" + "</kbd></br>");
-        }
-        //连接错误时触发
-        socket.onerror = function(evt) {
-            $("#content").append("<kbd>" + "ERROR!" + "</kbd></br>");
-        }
-    }
-    //按下回车键时触发发送消息方法
-    document.onkeydown = function(event){
-        var e = event || window.event || arguments.callee.caller.arguments[0];
-        if(e && e.keyCode == 13){ // enter键
-            emit();
-        }
+  function emit() {
+    var text = $("#msg").val();
+    var msg = {
+      "message" : text,
+      "color" : "#CECECE",
+      "bubbleColor" : "#2E2E2E",
+      "fontSize" : "14",
+      "fontType" : "微软雅黑"
     };
-    
+
+    msg = JSON.stringify(msg);
+
+    // 向服务端发送消息
+    socket.send(msg);
+
+    // 将自己发送的消息内容静态加载到html上，服务端实现自己发送的消息不会推送给自己
+    $("#content").append("<kbd style='color: #" + "CECECE" + ";float: right;clear: right; font-size: " + 12 + ";'>" + text +  "</kbd><br/>");
+
+    // 将消息文本框清空
+    $("#msg").val("");
+  } 
+
+  $(function() {
+    // 初始化加载listen方法
+    listen();
+  })
+  
+  function listen() {
+    // 打开连接时触发
+    socket.onopen = function() {
+      $("#content").append("<kbd>Welcome!</kbd></br>");
+    };
+
+    // 收到消息时触发
+    socket.onmessage = function(evt) {
+      var data = JSON.parse(evt.data);
+      $("#content").append("<kbd style='color: #" + data.color + "; font-size: " + data.fontSize + ";margin-top: 10px;'>" + data.userName + " Say: " + data.message + "</kbd></br>");
+    };
+
+    // 关闭连接时触发
+    socket.onclose = function(evt) {
+      $("#content").append("<kbd>" + "Close!" + "</kbd></br>");
+    }
+
+    // 连接错误时触发
+    socket.onerror = function(evt) {
+      $("#content").append("<kbd>" + "ERROR!" + "</kbd></br>");
+    }
+  }
+
+  // 按下回车键时触发发送消息方法
+  document.onkeydown = function(event){
+    var e = event || window.event || arguments.callee.caller.arguments[0];
+    //  keyCode == 13 enter键
+    if(e && e.keyCode == 13){ 
+      emit();
+    }
+  };
 });
